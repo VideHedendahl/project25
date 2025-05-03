b50630ebc05c571e1ce5fda957348ebe4d1a26ff
VideHedendahl <vide.hedendahl@elev.ga.ntig.se>
2025-05-03 16:14:46 +0200

    Skapat funktioner till db och db-hash samt skapat en before som kollar ifall användaren är inloggad

--- app.rb
+++ app.rb
@@ -6,14 +6,28 @@ require 'bcrypt'
 
 enable :sessions
 
+
+before('/protected/*') do
+  p "These are protected_methods"
+  if session[:id] ==  nil
+    redirect('/ingeninlog')
+  end
+end
+
+def connect_to_db(path)
+  db = SQLite3::Database.new(path)
+  db.results_as_hash = true
+  return db
+end
+
+
 get('/')  do
   slim(:start)
 end
 
 get('/products') do
   user_id = session[:id]
-  db = SQLite3::Database.new("db/chinook-crud.db")
-  db.results_as_hash = true
+  db = connect_to_db('db/chinook-crud.db')
   result = db.execute("SELECT * FROM products")
  
   if user_id == nil
@@ -23,42 +37,31 @@ get('/products') do
   end
 end
 
-get('/products/saved') do
+get('/protected/products/saved') do
   user_id = session[:id]
 
-  db = SQLite3::Database.new("db/chinook-crud.db")
-  db.results_as_hash = true
+  db = connect_to_db('db/chinook-crud.db')
   result = db.execute("SELECT * FROM products")
   result2 = db.execute("SELECT * FROM saved")
-  
-  if user_id == nil
-    slim(:"InlgEror")
-  else
-    slim(:"products/savedindex",locals:{products:result,saved:result2,user_id:user_id})
-  end
+
+  slim(:"products/savedindex",locals:{products:result,saved:result2,user_id:user_id})
 end
 
-get('/products/new') do
-  user_id = session[:id]
-
-  if user_id == nil
-    slim(:"InlgEror")
-  else
+get('/protected/products/new') do
     slim(:"products/new")
-  end
 end
 
 post('/products/new') do
   title = params[:title]
   company_id = params[:company_id].to_i
-  db = SQLite3::Database.new("db/chinook-crud.db")
+  db = connect_to_db('db/chinook-crud.db')
   db.execute("INSERT INTO products (Title, CompanyId) VALUES (?,?)",[title, company_id])
   redirect('/products')
 end
 
 post('/products/:id/delete') do
   id = params[:id].to_i
-  db = SQLite3::Database.new("db/chinook-crud.db")
+  db = connect_to_db('db/chinook-crud.db')
   db.execute("DELETE FROM products WHERE ProductsId = ?",id)
   redirect('/products')
 end
@@ -66,37 +69,30 @@ end
 post('/products/:id/save') do
   products_id = params[:id].to_i
   user_id = session[:id]
-
-  if user_id == nil
-    redirect('/products/saved')
-  end
-
-  db = SQLite3::Database.new("db/chinook-crud.db")
+  db = connect_to_db('db/chinook-crud.db')
   db.execute("INSERT INTO saved (id, product) VALUES (?,?)",[user_id, products_id])
   redirect('/products')
 end
 
 post('/saved/:row_number/delete') do
   row_number = params[:row_number].to_i
-
-  db = SQLite3::Database.new("db/chinook-crud.db")
+  db = connect_to_db('db/chinook-crud.db')
   db.execute("DELETE FROM saved WHERE row_number = ?", [row_number])
-  redirect('/products/saved')
+  redirect('/protected/products/saved')
 end
 
 post('/products/:id/update') do
   id = params[:id].to_i
   title = params[:title]
   company_id = params[:CompanyId].to_i
-  db = SQLite3::Database.new("db/chinook-crud.db")
+  db = connect_to_db('db/chinook-crud.db')
   db.execute("UPDATE products SET Title=?,CompanyId=? WHERE ProductsId = ?",[title, company_id, id])
   redirect('/products')
 end
 
 get('/products/:id/edit') do
   id = params[:id].to_i
-  db = SQLite3::Database.new("db/chinook-crud.db")
-  db.results_as_hash = true
+  db = connect_to_db('db/chinook-crud.db')
   result = db.execute("SELECT * FROM products WHERE ProductsId = ?",id).first
   slim(:"products/edit",locals:{result:result,id:id})
 end
@@ -112,7 +108,7 @@ post('/users/new') do
 
     if (password == password_confirm)
         password_digest = BCrypt::Password.create(password)
-        db = SQLite3::Database.new("db/chinook-crud.db")
+        db = connect_to_db('db/chinook-crud.db')
         db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",[username,password_digest])
         redirect('/registrera')
     else
@@ -128,8 +124,7 @@ post('/login') do
     username = params[:username]
     password = params[:password]
 
-    db = SQLite3::Database.new("db/chinook-crud.db")
-    db.results_as_hash = true
+    db = connect_to_db('db/chinook-crud.db')
     result = db.execute("SELECT * FROM users WHERE username = ?",username).first
     pwdigest = result["pwdigest"]
     id = result["id"]
@@ -149,11 +144,12 @@ end
 
 get('/products/:id') do
   id = params[:id].to_i
-  db = SQLite3::Database.new("db/chinook-crud.db")
-  db.results_as_hash = true
+  db = connect_to_db('db/chinook-crud.db')
   result = db.execute("SELECT * FROM products WHERE ProductsId = ?",id).first
   result2 = db.execute("SELECT Name FROM companies WHERE CompanyID IN (SELECT CompanyId FROM products WHERE ProductsId = ?)",id).first
   slim(:"products/show",locals:{result:result,result2:result2})
 end
 
-
+get('/ingeninlog') do
+  slim(:"InlgEror")
+end
